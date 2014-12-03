d2758c7dff78f2797d20d78d63f531f9a1a4a403
Devansh326 <devansh.m.kaushik@gmail.com>
2014-12-04 00:34:43 +0530

    Create boost_woker_threaad.cpp
    Simple Boost worker thread wrapper to understand boost thread and function pointers

---
+++ boost_woker_threaad.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include <iostream>
+#include <boost/thread.hpp>
+#include <boost/date_time.hpp>
+
+void MYFUNC(void *user_data)
+{
+
+	std::cout << std::endl <<"MYFUNC: running" << std::endl;
+	std::cout << (char *)user_data << std::endl;
+	boost::posix_time::seconds workTime(3);
+
+	// Pretend to do something useful...
+	boost::this_thread::sleep(workTime);
+
+	std::cout << "MYFUNC: finished" << std::endl;
+}
+
+class PCWorkerThread
+{
+public:
+	void Create(void(*Worker_thread)(void *), void *data);
+	boost::thread *workerThread;
+};
+
+void PCWorkerThread::Create(void(*Worker_thread)(void *),void *data)
+{
+	workerThread = new boost::thread(Worker_thread, data);
+}
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "main: startup" << std::endl;
+	char *a = new char;
+	a = "this is a test";
+	PCWorkerThread obj;
+	obj.Create(MYFUNC,(void *)a);
+	std::cout << " main: waiting for thread " <<std::endl;
+	
+	obj.workerThread->join();
+
+	std::cout << "main: done" << std::endl;
+	fflush(stdin);
+	getchar();
+	return 0;
+}
